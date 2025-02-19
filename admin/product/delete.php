a084b554e38ce4fc52f75a7b9c17a20378db6ec5
Wldnz <wildanofficial32@gmail.com>
2025-02-19 14:55:53 +0700

    Update delete.php

--- admin/product/delete.php
+++ admin/product/delete.php
@@ -8,6 +8,7 @@ if(!isAdmin()) {goToLoginPage(); die();}
 $url = rootLocation();
 
 if(isset($_POST["id"]) && $_POST["id"]){
+    $id = $_POST["id"];
     $result = $conn->query("DELETE from produk where id='$id'");
 
     if($result){
